f5df8e1cf3eb63c390cf73ef2d87ed059f332a75
Lucky Adogun <meetluckyadogun@gmail.com>
2023-06-25 13:29:03 +0100

    updated urls.py

--- djtest/urls.py
+++ djtest/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path("", views.index),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
